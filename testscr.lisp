b09ef6b0f68954c1a15e233d029db5335f7cf29b
Masataro Asai <guicho2.71828@gmail.com>
2015-06-25 10:51:27 +0900

    simplify testscr

--- testscr.lisp
+++ testscr.lisp
@@ -2,18 +2,12 @@
 (in-package :cl-user)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload :fiveam))
-
-(defun test (sys)
-  (handler-case
-      (progn
-        (ql:quickload sys)
-        (asdf:load-system sys)
-        (eval (read-from-string "(fiveam:run! 'arrow-macros-test::all)")))
-    (serious-condition (c)
-      (describe c)
-      (uiop:quit 2))))
+  (ql:quickload :arrow-macros-test))
 
 (uiop:quit (if (every #'fiveam::TEST-PASSED-P
-                      (test :arrow-macros))
+                      (handler-case
+                          (fiveam:run! 'arrow-macros-test::all)
+                        (serious-condition (c)
+                          (describe c)
+                          (uiop:quit 2))))
                0 1))
