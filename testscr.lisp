01e733dbc3e470da46fd55214b004728e57b517b
hipeta <rhipeta@gmail.com>
2016-09-12 05:04:56 +0900

    Fix bug: testscr errors because of fiveam:run! returning t/nil in recent version

--- testscr.lisp
+++ testscr.lisp
@@ -4,10 +4,9 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (ql:quickload :arrow-macros-test))
 
-(uiop:quit (if (every #'fiveam::TEST-PASSED-P
-                      (handler-case
-                          (fiveam:run! 'arrow-macros-test::all)
-                        (serious-condition (c)
-                          (describe c)
-                          (uiop:quit 2))))
+(uiop:quit (if (handler-case
+                   (fiveam:run! 'arrow-macros-test::all)
+                 (serious-condition (c)
+                   (describe c)
+                   (uiop:quit 2)))
                0 1))
